ae9fe25d718b70e23280a811ed5c9c0fba3e6eeb
小张想上天 <zhangzhenmin@skyet.cn>
2024-05-15 14:21:08 +0800

    一键修改图片类型功能完成 一键修改图片宽高功能完成

--- PictureProcessing/ImageTool.cs
+++ PictureProcessing/ImageTool.cs
@@ -4,16 +4,21 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Formats.Webp;
 using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Formats.Bmp;
+using System.Collections.Generic;
 
 namespace PictureProcessing
 {
-    internal class ImageTool
+    internal class ImageTool : Tool
     {
+        List<string> image_adjuster = ["裁剪", "填充", "盒子填充", "最大", "最小", "拉伸", "手动",];
+        List<string> image_position = ["中心点", "顶部", "底部", "左侧", "右侧", "左上角", "右上角", "右下角", "左下角",];
+
         // 输入和输出文件夹路径
-        string inputFolder = @"C:\Users\zhangzhenmin\Desktop\图片";
-        string outputFolder = @"C:\Users\zhangzhenmin\Desktop\图片\output";
+        string inputFolder = @".\";
+        string outputFolder = @".\output";
         // 要处理的文件扩展名
-        string[] extensions = { "*.jpg", "*.jpeg", "*.png", "*.bmp", "*.webp" };
+        string[] extensions = { "*.jpg", "*.jpeg", "*.png", "*.webp" };
         protected void UpdateImageType(string key)
         {
             try
@@ -30,10 +35,14 @@ namespace PictureProcessing
                 {
                     imageFiles.AddRange(Directory.GetFiles(inputFolder, extension));
                 }
+                Console.WriteLine($"检测到可处理图片数量：{imageFiles.Count}");
+
 
                 // 遍历每个图片文件并进行处理
                 foreach (string imageFile in imageFiles)
                 {
+                    Console.WriteLine($"正在处理图片：{Path.GetFileNameWithoutExtension(imageFile) + Path.GetExtension(imageFile)}");
+
                     // 使用 Image.Load 方法加载图片
                     using (Image<Rgba32> image = Image.Load<Rgba32>(imageFile))
                     {
@@ -62,18 +71,19 @@ namespace PictureProcessing
                 Console.WriteLine($"处理图片时出错：{ex.Message}");
             }
         }
-        public void Test()
-        {
-            // 输入和输出文件夹路径
-            string inputFolder = @"C:\Users\zhangzhenmin\Desktop\图片";
-            string outputFolder = @"C:\Users\zhangzhenmin\Desktop\图片\output";
-
+        protected void UpdateImageSize() {
             // 目标尺寸
-            int targetWidth = 800;
-            int targetHeight = 600;
+            Console.Write("请输入宽度:");
+            int targetWidth = InputIntValue();
+
+            Console.Write("请输入高度:");
+            int targetHeight = InputIntValue();
+
+            Console.WriteLine("请选择图像显示类型");
+            string image_adjuster_key = SelectMenusNumber(image_adjuster);
 
             try
-            {
+                {
                 // 确保输出文件夹存在
                 if (!Directory.Exists(outputFolder))
                 {
@@ -81,35 +91,109 @@ namespace PictureProcessing
                 }
 
                 // 获取输入文件夹下所有图片文件
-                string[] imageFiles = Directory.GetFiles(inputFolder, "*.png");
-
-                // 遍历每个图片文件并进行处理
-                foreach (string imageFile in imageFiles)
+                List<string> imageFiles = new List<string>();
+                foreach (string extension in extensions)
                 {
-                    // 使用 Image.Load 方法加载图片
-                    using (Image<Rgba32> image = Image.Load<Rgba32>(imageFile))
+                    imageFiles.AddRange(Directory.GetFiles(inputFolder, extension));
+                }
+                Console.WriteLine($"检测到可处理图片数量：{imageFiles.Count}");
+
+                if (image_adjuster_key == "7") {
+                    string image_position_key = SelectMenusNumber(image_position);
+
+                    // 遍历每个图片文件并进行处理
+                    foreach (string imageFile in imageFiles)
                     {
-                        // 调用 Resize 方法进行图片缩放，保持原始长宽比
-                        image.Mutate(x => x.Resize(new ResizeOptions
-                        {
-                            Size = new Size(targetWidth, targetHeight),
-                            Mode = ResizeMode.Stretch // 允许变形
-                        }));
+                    Console.WriteLine($"正在处理图片：{Path.GetFileNameWithoutExtension(imageFile) + Path.GetExtension(imageFile)}");
+                        // 使用 Image.Load 方法加载图片
+                        using (Image<Rgba32> image = Image.Load<Rgba32>(imageFile))
+                            {
+                                // 调用 Resize 方法进行图片缩放，允许变形
+                                image.Mutate(x => x.Resize(new ResizeOptions
+                                {
 
-                        // 构造输出文件路径
-                        string outputFile = Path.Combine(outputFolder, Path.GetFileNameWithoutExtension(imageFile) + ".webp");
+                                    Size = new Size(targetWidth, targetHeight),
+                                    Mode = GetResizeMode(image_adjuster_key),
+                                    Position = GetAnchorPositionMode(image_position_key), // 手动指定目标位置有BUG
 
-                        // 保存处理后的图片到输出文件夹，指定  格式
-                        image.Save(outputFile, new PngEncoder());
+                                }));
+
+                                // 获取文件扩展名并选择合适的编码器
+                                string extension = Path.GetExtension(imageFile).ToLower();
+                                string outputFile = Path.Combine(outputFolder, Path.GetFileNameWithoutExtension(imageFile) + extension);
+
+                                switch (extension)
+                                {
+                                    case ".jpg":
+                                    case ".jpeg":
+                                        image.Save(outputFile, new JpegEncoder());
+                                        break;
+                                    case ".png":
+                                        image.Save(outputFile, new PngEncoder());
+                                        break;
+                                    case ".webp":
+                                        image.Save(outputFile, new WebpEncoder());
+                                        break;
+                                    default:
+                                        throw new NotSupportedException($"不支持的文件格式: {extension}");
+                                }
+                            }
+
                     }
                 }
+                else
+                {
+                    // 遍历每个图片文件并进行处理
+                    foreach (string imageFile in imageFiles)
+                    {
+                    Console.WriteLine($"正在处理图片：{Path.GetFileNameWithoutExtension(imageFile) + Path.GetExtension(imageFile)}");
+                        // 使用 Image.Load 方法加载图片
+                        using (Image<Rgba32> image = Image.Load<Rgba32>(imageFile))
+                        {
+                            // 调用 Resize 方法进行图片缩放，允许变形
+                            image.Mutate(x => x.Resize(new ResizeOptions
+                            {
 
+                                Size = new Size(targetWidth, targetHeight),
+                                Mode = GetResizeMode(image_adjuster_key),
+
+                            }));
+
+                            // 获取文件扩展名并选择合适的编码器
+                            string extension = Path.GetExtension(imageFile).ToLower();
+                            string outputFile = Path.Combine(outputFolder, Path.GetFileNameWithoutExtension(imageFile) + extension);
+
+                            switch (extension)
+                            {
+                                case ".jpg":
+                                case ".jpeg":
+                                    image.Save(outputFile, new JpegEncoder());
+                                    break;
+                                case ".png":
+                                    image.Save(outputFile, new PngEncoder());
+                                    break;
+                                case ".webp":
+                                    image.Save(outputFile, new WebpEncoder());
+                                    break;
+                                default:
+                                    throw new NotSupportedException($"不支持的文件格式: {extension}");
+                            }
+                        }
+
+                    }
+                }
                 Console.WriteLine("图片处理完成！");
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"处理图片时出错：{ex.Message}");
             }
+        }
+
+        public void Test()
+        {
+
+
         }
     }
 }
---
+++ PictureProcessing/Tool.cs
@@ -0,0 +1,112 @@
+﻿using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureProcessing
+{
+    internal class Tool
+    {
+        internal string SelectMenusNumber(List<string> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{list[i]}");
+            }
+
+            Console.Write("请输入功能序号:");
+            string? key = Console.ReadLine();
+            if (int.TryParse(key, out int number))
+            {
+                if (number > 0 && number <= list.Count)
+                {
+                    return key;
+                }
+                else
+                {
+                    Console.WriteLine("选择无效");
+                    return SelectMenusNumber(list);
+                }
+            }
+            else
+            {
+                Console.WriteLine("选择无效");
+                return SelectMenusNumber(list);
+            }
+        }
+
+        internal ResizeMode GetResizeMode(string image_adjuster_key)
+        {
+            switch (image_adjuster_key)
+            {
+                case "1":
+                    return ResizeMode.Crop; // 把图像的中间部分剪下来，使其正好适应新尺寸。
+                case "2":
+                    return ResizeMode.Pad; // 在图像周围填充空白，使图像适应新尺寸。
+                case "3":
+                    return ResizeMode.BoxPad;  // 保持图像原大小不变，周围填充空白使其适应新尺寸
+                case "4":
+                    return ResizeMode.Max;// 调整图像尺寸，使其最大程度地适应新尺寸的边界。
+                case "5":
+                    return ResizeMode.Min; //调整图像尺寸，使其至少能覆盖新尺寸的一边。
+                case "6":
+                    return ResizeMode.Stretch; // 直接拉伸或压缩图像，使其完全适应新尺寸。
+                case "7":
+                    return ResizeMode.Manual; // 手动指定图像的位置和大小，不进行自动调整。
+                default:
+                    throw new NotSupportedException($"不支持的图像显示类型");
+            }
+        }
+        internal AnchorPositionMode GetAnchorPositionMode(string image_position_key)
+        {
+            switch (image_position_key)
+            {
+                case "1":
+                    return AnchorPositionMode.Center; // 图像的中心点
+                case "2":
+                    return AnchorPositionMode.Top; // 图像的顶部
+                case "3":
+                    return AnchorPositionMode.Bottom;  // 图像的底部
+                case "4":
+                    return AnchorPositionMode.Left;// 图像的左侧
+                case "5":
+                    return AnchorPositionMode.Right; // 图像的右侧
+                case "6":
+                    return AnchorPositionMode.TopLeft; // 图像的左上角
+                case "7":
+                    return AnchorPositionMode.TopRight; // 图像的右上角
+                case "8":
+                    return AnchorPositionMode.BottomRight; //  图像的右下角
+                case "9":
+                    return AnchorPositionMode.BottomLeft; //  图像的左下角
+                default:
+                    throw new NotSupportedException($"不支持的图像显示类型");
+            }
+        }
+
+        internal int InputIntValue()
+        {
+
+            string? value = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                Console.WriteLine("未提供输入.");
+                Console.Write("请重新输入:");
+                return InputIntValue();
+            }
+            if (int.TryParse(value, out int intValue))
+            {
+                return intValue; // 返回解析后的整数值
+            }
+            else
+            {
+                Console.WriteLine($"{value} 不是一个有效的数值.");
+                Console.Write("请重新输入:");
+                return InputIntValue();
+            }
+        }
+    }
+}
